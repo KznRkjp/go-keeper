e41bbab48d2f2f3d96cca7336cf38b1e988e7afc
Mikhail Martynenko <2424324@gmail.com>
2024-10-28 14:48:31 +0300

    + delete

---
+++ internal/database/deletedata.go
@@ -0,0 +1,59 @@
+package database
+
+import (
+	"context"
+
+	"github.com/KznRkjp/go-keeper.git/internal/middleware/mlogger"
+)
+
+func DeleteDataLP(recordId *string, userId *int, ctx context.Context) error {
+	insertDynStmt := `
+	DELETE FROM "logopass"
+	WHERE "id" = $1
+	AND "go_k_user_id" = $2`
+	_, err := db.ExecContext(ctx, insertDynStmt, recordId, userId)
+	if err != nil {
+		mlogger.Logger.Error(err.Error())
+		return err
+	}
+	return nil
+}
+
+func DeleteDataBC(recordId *string, userId *int, ctx context.Context) error {
+	insertDynStmt := `
+	DELETE FROM "bank_card
+	WHERE "id = $1
+	AND "go_k_user_id = $2`
+	_, err := db.ExecContext(ctx, insertDynStmt, recordId, userId)
+	if err != nil {
+		mlogger.Logger.Error(err.Error())
+		return err
+	}
+	return nil
+}
+
+func DeleteDataTxt(recordId *string, userId *int, ctx context.Context) error {
+	insertDynStmt := `
+	DELETE FROM "text_data"
+	WHERE "id = $1
+	AND "go_k_user_id = $2`
+	_, err := db.ExecContext(ctx, insertDynStmt, recordId, userId)
+	if err != nil {
+		mlogger.Logger.Error(err.Error())
+		return err
+	}
+	return nil
+}
+
+func DeleteDataBM(recordId *string, userId *int, ctx context.Context) error {
+	insertDynStmt := `
+	DELETE FROM "binary_data"
+	WHERE "id = $1
+	AND "go_k_user_id = $2`
+	_, err := db.ExecContext(ctx, insertDynStmt, recordId, userId)
+	if err != nil {
+		mlogger.Logger.Error(err.Error())
+		return err
+	}
+	return nil
+}
